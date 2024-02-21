e96c4d24c3dbdbc4e87c2eb6ad895a615f8a278b
Sohit666 <sohit9865@gmail.com>
2024-02-21 21:48:31 +0530

    Initial commit

---
+++ src/components/background.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css'; // Import your CSS file
+
+function App() {
+  return (
+    <div className="background-image-container">
+      {/* Your content goes here */}
+    </div>
+  );
+}
+
+export default App;
